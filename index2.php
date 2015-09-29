bff925c0e8e419bd89786bf96904f5faf193a0dc
googlecastle <tabadaeater123@gmail.com>
2015-09-29 11:28:43 -0700

    myapp

---
+++ index2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta name="viewport" content="width=device-width, initial-
+ scale=1">
+
+<link rel="stylesheet"href="jquery.mobile-1.4.5.min.css" />
+ <script type="text/javascript"
+ src="jquery-1.4.5.min.js"></script>
+ <script type="text/javascript"src="jquery.mobile-1.4.5.min.js"></script>
+
+ </head>
+ <body>
+ <div data-role="page" id="homePage">
+ <div data-role="header"><h1>Welcome</h1></div>
+ <div data-role="content">
+ <p>
+ Welcome to our first mobile web site. It's going to be the
+ best site you've ever seen. Once we get some content. And
+ a business plan. But the hard part is done!
+ </p>
+ <p>
+ You can also <a href="#aboutPage">learn more</a> about
+ Megacorp.
+ </p>
+ </div>
+ <div data-role="footer">
+ <h4>Copyright Megacorp &copy; 2013</h4>
+ </div>
+ </div>
+ <div data-role="page" id="aboutPage">
+ <div data-role="header"><h1>About Megacorp</h1></div>
+ <div data-role="content">
+ <p>
+ This text talks about Megacorp and how interesting it is.
+ Most likely though you want to
+ <a href="#homePage">return</a> to the home page.
+ </p>
+ </div>
+ <div data-role="footer">
+ <h4>Copyright Megacorp &copy; 2013/h4>
+ </div>
+ </div>
+ </body>
+</html>
